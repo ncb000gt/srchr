19470d04be01aa84416606015d0a68ec5739aeb8
Nick Campbell <nicholas.j.campbell@gmail.com>
2010-03-22 22:31:45 -0400

    Web and Flickr. Not pretty, code is moderately horrendous.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- src/js/srchr.js
+++ src/js/srchr.js
@@ -1,15 +1,57 @@
 var srchr = {
-  srchs: []
+  srchs: [],
+  updateSearchTerms: function() {
+    var terms = $("#search_terms");
+    terms.empty();
+    terms.append(
+      srchr.srchs.map(
+	function(i) {
+	  return "<li>"+i+"</li>";
+	}
+      ).join("")
+    );
+  },
+  updateSearchResults: function(results) {
+    if ('web' in results) {
+      var web_results = $("#web_results");
+      web_results.empty();
+      web_results.append(
+	$.map(results['web'],
+	      function(i) {
+		return '<li>'
+		  + '<a href="'+i.url+'">'+i.title+'</a>'
+		  + '<p>'+i['abstract']+'</p>'
+		  + '</li>';
+	      }
+	     ).join("")
+      );
+    }
+
+    if ('flickr' in results) {
+      var flickr_results = $("#flickr_results");
+      flickr_results.empty();
+      flickr_results.append(
+	$.map(results['flickr'],
+	      function(i) {
+		return '<li>'
+		  + '<a href="http://www.flickr.com/photos/'+i.owner+'/'+i.id+'/">'
+		  + '<img src="http://farm'+i.farm+'.static.flickr.com/'+i.server+'/'+i.id+'_'+i.secret+'_s.jpg" alt="'+i.title+'" />'
+		  + '</a>'
+		  + '</li>';
+	      }
+	     ).join("")
+      );
+    }
+  }
 };
 
 $(document).ready(
   function() {
-    alert('Lets start this piece.');
-
-
     $("#search_submit").click(
       function() {
 	var query = $("#search").val();
+	srchr.srchs.unshift(query);
+
 	$.yql(
 	  "SELECT * FROM search.web WHERE query='#{query}'",
 	  {
@@ -17,8 +59,34 @@ $(document).ready(
 	  },
 	  function (data) {
 	    console.log(data);
+	    if (!(data.error)) {
+	      srchr.updateSearchResults(
+		{
+		  'web': data.query.results.result
+		}
+	      );
+	    }
 	  }
 	);
+
+	$.yql(
+	  "SELECT * FROM flickr.photos.search WHERE text='#{query}' and has_geo='true'",
+	  {
+	    query: query
+	  },
+	  function (data) {
+	    console.log(data);
+	    if (!(data.error)) {
+	      srchr.updateSearchResults(
+		{
+		  'flickr': data.query.results.photo
+		}
+	      );
+	    }
+	  }
+	);
+
+	srchr.updateSearchTerms();
       }
     );
   }
